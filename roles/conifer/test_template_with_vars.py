da654e51649af1ab1275390462e9e51b0f5f1bc9
Ryan Doherty <tech@conical.org>
2024-11-19 16:56:52 -0500

    Initial commit

---
+++ roles/conifer/test_template_with_vars.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from copy import deepcopy
+from ansible.plugins.action import ActionBase
+from ansible.plugins.action.template import ActionModule as TemplateActionModule
+from ansible.errors import AnsibleError
+import json
+
+class ActionModule(TemplateActionModule, ActionBase):
+  TRANSFERS_FILES = False
+
+  def run(self, tmp=None, task_vars=None):
+    custom_vars = {
+    "project" : "Plasmo",
+    "fancy_project": '{{ project }}'
+    }
+    custom_vars['ansible_facts'] = task_vars['ansible_facts']
+    return super(ActionModule, self).run(tmp, custom_vars)
+
