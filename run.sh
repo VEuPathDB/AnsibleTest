4749c7123e2787b9a8b6a59b99102d90a5c9f14d
Ryan Doherty <tech@conical.org>
2024-11-20 13:44:44 -0500

    add debug

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 projectDir=$(dirname `realpath $0`)
 
 export ANSIBLE_CONFIG=$projectDir/conifer.cfg
-ansible-playbook -i localhost, -e outputFile=$(pwd)/output.xml $projectDir/playbook.yml
+ansible-playbook -i localhost, -e outputFile=$(pwd)/output.xml -vvv $projectDir/playbook.yml
